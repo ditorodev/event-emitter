892f80cd6a1eb69910047331848a912df82450b2
Juan Di Toro <ditorojuanf@gmail.com>
2017-12-27 13:52:06 -0400

    initial commir

---
+++ list.h
@@ -0,0 +1,40 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct client node_c;
+typedef struct event node_e;
+typedef struct center Centro;
+typedef struct centerList node_center;
+typedef struct senderList node_s;
+
+struct client {
+    void *client;
+    void (*f) (void *);
+    struct client *next;
+};
+
+struct event {
+    char name[100];
+    node_s *senders;
+    node_center *centers;
+    struct event *next;
+};
+
+struct center {
+    char name[100];
+    node_e *events;
+    struct center *next;
+};
+
+struct centerList{
+    Centro **center;
+    struct centerList *next;
+};
+
+struct senderlist {
+    void *sender;
+    node_c *clients;
+    struct senderList *next;
+};
+
+#endif
---
+++ main.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+void error(int n){
+    switch(n){
+        case 1:
+            printf("\n ERROR: Uno de los centros no existe \n");
+            break;
+
+    }
+}
+
+Centro *CENTROS;
+
+void newCenter(char *name) {
+
+    Centro *new = (Centro *) malloc(sizeof(Centro));
+    if(!CENTROS){ // NOS ASEGURAMOS QUE EXISTA ALGUIEN EN LA LISTA DE CENTROS
+        CENTROS = new;
+        strcpy(new->name, name);
+        return;
+    }
+
+    Centro *aux = CENTROS;
+    strcpy(new->name, name);
+    aux = CENTROS;
+    while(aux->next) aux = aux->next;
+    aux->next = new;
+}
+
+Centro * locateCenter(char *center){
+    Centro *aux = CENTROS;
+    
+    while(aux && strcmp(center, aux->name)) aux = aux->next;    
+    return aux;
+}
+node_e * locateEvent(Centro *center, char *event){
+    node_e *aux = center->events;
+
+    while(aux && strcmp(event, aux->name)) aux = aux->next;
+    printf("\n EVENTO LOCALIZADO %s \n", aux->name);
+    return aux;
+}
+
+void observeCenterFromCenter (char *centerA, char *centerB, char *event){
+    Centro *cA = locateCenter(centerA);
+    Centro *cB = locateCenter(centerB);
+    node_e *aux;
+    node_e *new = locateEvent(cA, event);
+
+    if(!cA || !cB){ 
+        error(1);
+        return;
+    }
+    // asociamos el evento al centro
+    aux = cA->events;
+    if(!new){  
+        new = (node_e *)malloc(sizeof(node_e));
+        strcpy(new->name, event);
+        printf("\nNUEVO EVENTO A REGISTAR EN \n %s \n %s\n", cA->name,new->name);
+        if(!aux) cA->events = new;
+        else {
+            while(aux->next) aux = aux->next;
+            aux->next = new;
+        }
+    }
+
+    printf("\n\n centro 1: %s encontrado %s \n\n", centerA, cA->name);
+    printf("\n\n centro 2: %s encontrado %s \n\n", centerB, cB->name);
+        
+    // asociamos el centro b al evento
+    node_center *aux1 = new->centers;    
+    if(!aux1){ 
+        aux1 = (node_center *)malloc(sizeof(node_center));
+        aux1->center = &cB;
+        new->centers = aux1;    
+    }else{
+        while(aux1->next) aux1 = aux1->next;
+        aux1->next = (node_center *) malloc(sizeof(node_center));
+        aux1->next->center = &cB;
+    }
+    printf("\nCENTRO \n %s\n REGISTRADO AL EVENTO \n %s\n", (*new->centers->center)->name, new->name); 
+    printf("\nEVENTO %s registrado exitosamente\n", cA->events->name);
+
+    
+}
+
+// void observeCenterFromClient(char *centerA, void *client, void *sender, char *event, void (* methodToCall)(void *)){
+//     Centro *c = locateCenter(centerA);
+//     node_e *new = locateEvent(c, event);
+//     node_s *aux_s;
+//     if (!new) new = (node_e *) malloc(sizeof(node_e));
+//     else{
+//         aux_s = 
+
+//     }
+
+
+
+
+// }
+
+int main (void) {
+    char nombre[100];
+    int i = 0;
+    for(; i <5; i++){
+        scanf("%100s", nombre);
+        newCenter(nombre);
+    };
+
+    Centro *aux = CENTROS;
+    while(aux) {
+        printf("---> %s", aux->name);
+        aux = aux->next;
+    }
+
+    printf("\nOBSERVE CENTER FROM CENTER\n");
+    observeCenterFromCenter("A", "B", "SEXO ANAL");
+    observeCenterFromCenter("A", "D", "SEXO ANAL");
+    observeCenterFromCenter("A", "C", "SEXO ANAL");
+
+    node_center *aux1;
+    node_e *aux2;
+    aux = CENTROS;
+    while(aux){
+        printf("\n Centros asociados a %s \n", aux->name);
+        if(!(aux->events)){ 
+            aux = aux->next;
+            continue;
+            }
+        aux2 = aux->events;
+        while(aux2){
+            printf("  CENTROS REGISTRADOS AL EVENTO %s", aux2->name);
+            aux1 = aux2->centers;
+            while(aux1){
+                printf("\n   %s \n", (*aux1->center)->name);
+                aux1 = aux1->next;
+            }
+            aux2 = aux2->next;
+        }
+        aux = aux->next;
+    }
+    return 0;
+}
